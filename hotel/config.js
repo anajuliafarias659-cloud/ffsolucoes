b5468a68ad59d2078340e8fbc8b0eaca61e527e3
anajuliafarias659-cloud <anajuliafarias659@gmail.com>
2026-01-15 01:55:45 -0300

    Create config.js

---
+++ hotel/config.js
@@ -0,0 +1,7 @@
+// config.js
+var SUPABASE_URL = "https://pdajixsoowcyhnjwhgpc.supabase.co",
+var SUPABASE_KEY = "sb_publishable_LatlFlcxk6IchHe3RNmfwA_9Oq4EsZw"
+
+function getUsuario() {
+  return JSON.parse(sessionStorage.getItem("usuario"));
+}
