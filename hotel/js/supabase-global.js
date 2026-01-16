9f40e3ba67268f608ab0f46f9d90e8f73f077443
anajuliafarias659-cloud <anajuliafarias659@gmail.com>
2026-01-16 00:06:40 -0300

    Update supabase-global.js

--- hotel/js/supabase-global.js
+++ hotel/js/supabase-global.js
@@ -1,10 +1,7 @@
 if (!window.supabaseClient) {
-  const SUPABASE_URL = "https://pdajixsoowcyhnjwhgpc.supabase.co";
-  const SUPABASE_KEY = "sb-publishable-LAtlFlcxk6IchHe3RNmfwA_9Oq4EsZw";
-
-  // Usa o objeto global da biblioteca carregada
+  const SUPABASE_URL = "https://pdajixsoowcyhnjwhgpc.supabase.co",
+  const SUPABASE_KEY = "sb_publishable_LatlFlcxk6IchHe3RNmfwA_9Oq4EsZw"
   const { createClient } = window.supabase;
   window.supabaseClient = createClient(SUPABASE_URL, SUPABASE_KEY);
-
   console.log("✅ Supabase global inicializado com sucesso.");
 }
