763a8497c73a73b4e232fc657088ec6138baffb8
anajuliafarias659-cloud <anajuliafarias659@gmail.com>
2026-01-15 23:25:44 -0300

    Update and rename supabase-client.js to supabase-global.js

--- hotel/js/supabase-client.js
+++
@@ -1,5 +0,0 @@
-// Cliente global do Supabase
-const sb = supabase.createClient(
-  "https://pdajixsoowcyhnjwhgpc.supabase.co",
-  "sb_publishable_LatlFlcxk6IchHe3RNmfwA_9Oq4EsZw"
-);
---
+++ hotel/js/supabase-global.js
@@ -0,0 +1,5 @@
+if (!window.supabaseClient) {
+  const SUPABASE_URL = "https://pdajixsoowcyhnjwhgpc.supabase.co";
+  const SUPABASE_KEY = "sb_publishable_LAtlFlcxk6IchHe3RNmfwA_9Oq4EsZw";
+  window.supabaseClient = window.supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
+}
